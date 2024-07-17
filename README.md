99d6b79491d54da45234a72a94e53efa9263474f
throd272727 <throd27@nate.com>
2024-07-17 17:13:44 +0900

    feat: 뉴스 API 연동 실습

--- README.md
+++ README.md
@@ -1,22 +1,35 @@
 # 1 카드 뉴스 API 연동하기
 
-## 1.1 비동기 작업의 이해
+## 1.1 비동기 작업
 
 - Fetch API 기본형
 
-```
+```js
 window.addEventListener("load", function () {
-  // 1. json 호출하고 성공하면
-  fetch("./api/card.json")
-    .then((response) => {
-      console.log(response);
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+ fetch(url)
+  .then((response) => {
+   console.log(response);
+   return response.json();
+  })
+  .then((data) => {
+   console.log(data);
+  })
+  .catch((error) => {
+   console.log(error);
+  });
 });
+```
+
+## 1.2 두줄 이상 말줄임
+
+```css
+선택자 {
+ display: -webkit-box;
+ -webkit-box-orient: vertical;
+ overflow: hidden;
+ text-overflow: ellipsis;
+ -webkit-line-clamp: 3;
+ line-height: 1.5;
+ max-height: calc(1.5em * 3);
+}
 ```
---
+++ js/news.js
@@ -0,0 +1,63 @@
+window.addEventListener("load", function () {
+  const apiKey = config.apiKey;
+  const newsUrl = `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${apiKey}`;
+  // 1. api 호출하고 성공하면
+  fetch(newsUrl)
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      const result = makeHtmlTag(data);
+      return result;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  // html 태그 생성 함수
+  function makeHtmlTag(data) {
+    console.log(data.articles);
+
+    const articles = data.articles;
+    // html 태그 글자
+    let tag = "";
+
+    articles.forEach((article) => {
+      const { title, description, url, urlToImage, publishedAt } = article;
+
+      const tempTag = `
+       <div class="card-wrap">
+       <div class="thumbnail">
+        <a href="${url}" target="_blank" rel="noopener noreferrer">
+         <img src="${urlToImage}" alt="thumbnail" />
+        </a>
+       </div>
+   
+       <div class="content">
+        <h2>
+         <a href="${url}" target="_blank" rel="noopener noreferrer">${title}</a>
+        </h2>
+        <p class="desc">${description}</p>
+        <p class="date">${publishedAt}</p>git
+       </div>
+      </div>
+      `;
+
+      tag += tempTag;
+    });
+
+    // .card-wrap 요소가 없는 경우, 새로운 컨테이너 생성
+    let newsContainer = document.querySelector("#news-container");
+    const wrapElement = document.querySelector(".wrap");
+    if (!newsContainer) {
+      newsContainer = document.createElement("div");
+      newsContainer.id = "news-container";
+      wrapElement.appendChild(newsContainer);
+    }
+
+    // html에 배치
+    newsContainer.innerHTML = tag;
+    return newsContainer;
+  }
+});
